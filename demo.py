2ba9303212ac76d4426ffec1020f6c9eb9b6e5d6
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-07-14 15:42:18 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,9 +2,9 @@
 
 from xyz_canvas.canvas import make_objects
 
-def on_complete_cb(lines):
+def on_complete(lines):
     print("Object returned: ")
     for l in lines:
         print(l)
 
-demo_3D_builder = make_objects(on_complete_cb)
+demo_3D_builder = make_objects(on_complete_cb = on_complete)
